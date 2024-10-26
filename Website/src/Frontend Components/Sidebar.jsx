98853e5263dc2a45c2211d5762975ef749d5638b
Mihai <mihai.muscalu@student.htldornbirn.at>
2024-10-26 17:08:44 +0200

    File Structure and Sidebar Structure/Buttons done

---
+++ Website/src/Frontend Components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <button>Startseite</button>
+      <button>Strele</button>
+      <button>Kompetenzen</button>
+      <button>Jobs</button>
+      <button>Kontakt</button>
+      <button>Datenschutz</button>
+      <button>Shop</button>
+      <button>Bassena Bad</button>
+      <button>ecoturbino</button>
+    </div>
+  );
+};
+
+export default Sidebar;
