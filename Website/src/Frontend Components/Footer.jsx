bb75627a4be23cd42126e8effb9cee0e67e6dd63
Mihai <mihai.muscalu@student.htldornbirn.at>
2024-10-30 16:04:35 +0100

    design polish

--- Website/src/Frontend Components/Footer.jsx
+++ Website/src/Frontend Components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-      <footer className="bg-white text-center text-green-700 py-2 text-sm">
+      <footer className="bg-white text-center text-green-700 py-6 m-8 text-sm">
         <p>Strele Installationen GmbH | Bindung 14 | 6600 Breitenwang | Tel: +43 5672 63969 | office@strele.at</p>
       </footer>
     );
--- Website/src/Frontend Components/Table.jsx
+++ Website/src/Frontend Components/Table.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Table() {
   return (
     <div className="w-3/4 mx-auto"> {/* Adjusted width and centered */}
-      <table className="w-full border border-green-600">
+      <table className="w-5/6 border border-green-600">
         <thead>
           <tr className="bg-white text-green-600 font-bold text-2xl">
             <th className="border-3 border-green-600 py-5 px-6">Werkzeug</th>
