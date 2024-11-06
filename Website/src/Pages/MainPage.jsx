9e4d635559f7c2f932920136f618e58f36e88b11
Mihai <mihai.muscalu@student.htldornbirn.at>
2024-11-06 17:18:11 +0100

    another audit fix

---
+++ Website/src/Pages/MainPage.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Table from './Frontend Components/Table';
+import Footer from './Frontend Components/Footer';
+import Image from  "./Frontend Components/Images/strele.png"
+import Sidebar from './Frontend Components/Sidebar';
+
+const Home = () => {
+    return (
+        <div className="min-h-screen bg-white">
+          {/* Header at the top, spanning full width */}
+          <header className="bg-green-600 text-white flex items-end justify-end py-2 mt-20">
+            {/* Placeholder for possible left-aligned content */}
+            <div className="w-10"></div>
+    
+            {/* Centered text */}
+            <div className="text-center mr-2">
+              <h1 className="text-base font-bold">Wärmstes zu empfehlen.</h1>
+            </div>
+    
+            
+            {/* Right-aligned logo */}
+            <img src={Image} alt="Logo" className="h-20 mr-48" />
+          </header>
+    
+          {/* Main content area with sidebar and table */}
+          <div className="flex">
+            {/* Sidebar on the left */}
+            <Sidebar />
+            
+            {/* Main content on the right */}
+            <div className="flex-1 p-4">
+              <main className="flex justify-center items-center mt-10">
+                {/* Main Table */}
+                <Table />
+              </main>
+              {/* Footer contact information */}
+              <Footer />
+            </div>
+          </div>
+        </div>
+      );
+};
+
+export default Home;
